91e1116bbf59689bc31318703a496591e5b22b9f
Erik Vogler <erikvogler@gmail.com>
2021-05-25 14:54:53 +0200

    started with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |x|
+  Boat.create(
+  name: movie['title'],
+  description: movie['overview'],
+  pictures_url: "#{base_poster_url}#{movie['backdrop_path']}",
+  location: movie['vote_average']
+  price_per_night:
+
+  )
+end
