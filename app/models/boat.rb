8189cb76a629eb562775197408e9f6af5e7d4cce
Maria Vlad <maaria.vld@gmail.com>
2021-05-25 16:21:19 +0200

    User model description length from 50 to 20 char

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,5 @@
 class Boat < ApplicationRecord
   belongs_to :user
   validates :name, :location, :price_per_night, :description, :pictures_url, presence: true
-  validates :description, length: { minimum: 50, too_short: "minimum is %{count} characters"}
+  validates :description, length: { minimum: 20, too_short: "minimum is %{count} characters"}
 end
