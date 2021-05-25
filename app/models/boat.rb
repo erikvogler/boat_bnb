e11ee6d3799bc768b8333dd5e6a3969505a979bc
Maria Vlad <maaria.vld@gmail.com>
2021-05-25 12:23:02 +0200

    Boat model validations for description

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,5 @@
 class Boat < ApplicationRecord
   belongs_to :user
-  validates :name, :location, :price_per_night, :pictures_url, presence: true
+  validates :name, :location, :price_per_night, :description, :pictures_url, presence: true
+  validates :description, length: { minimum: 50, too_short: "minimum is %{count} characters"}
 end
