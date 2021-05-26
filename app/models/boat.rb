f612dfd0369c7c07ca713f5614a94c269e3a06df
Cristina Oechsle <crisoechsle@gmail.com>
2021-05-26 18:08:22 +0200

    geocoding in process

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,6 @@
 class Boat < ApplicationRecord
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
   belongs_to :user
   validates :name, :location, :price_per_night, :description, :pictures_url, presence: true
   validates :description, length: { minimum: 20, too_short: "minimum is %{count} characters"}
