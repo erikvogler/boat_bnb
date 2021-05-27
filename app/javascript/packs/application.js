4c5316d1e26a82d4cd5d3bda974771e81fa5ce1c
Maria Vlad <maaria.vld@gmail.com>
2021-05-27 17:01:43 +0200

    Book a boat form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import { initFlatpickr } from "../plugins/flatpickr";
 
 Rails.start()
 Turbolinks.start()
@@ -21,10 +22,12 @@ ActiveStorage.start()
 // External imports
 import "bootstrap";
 
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initFlatpickr();
 });
